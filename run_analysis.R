f1601f06f28b5e116d990dae7311a52a6ad88651
Yongxiao Zhou <jackzhouyongxiao@gmail.com>
2020-06-23 16:49:16 -0700

    1

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,25 @@
 #run_analysis
+# extract training data and labels
+setwd("D:/Users/Yongxiao/datasciencecoursera/ProgrammingAssignment4/UCI HAR Dataset/train")
+train<-read.table("X_train.txt",sep = "")
+trainsub<-read.table("subject_train.txt")
+trainlab<-read.table("y_train.txt")
+
+# extract test data and labels
+setwd("D:/Users/Yongxiao/datasciencecoursera/ProgrammingAssignment4/UCI HAR Dataset/test")
+test<-read.table("X_test.txt",sep = "")
+testsub<-read.table("subject_test.txt")
+testlab<-read.table("y_test.txt")
+
+# merge train and test data together
+data<-rbind(train,test)
+datasub<-rbind(trainsub,testsub)
+datalab<-rbind(trainlab,testsub)
+
+#read feature.txt
+setwd("D:/Users/Yongxiao/datasciencecoursera/ProgrammingAssignment4/UCI HAR Dataset")
+feature<-read.table("features.txt")
+
+#add column name
+colnames(data)<-feature[,2]
+sublocation<-grep("mean|std",feature$V2)
